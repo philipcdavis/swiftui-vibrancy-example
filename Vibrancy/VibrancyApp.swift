5482889213adcf72c022d66821d39c0b4c675899
Philip Davis <philipcdavis11@gmail.com>
2021-01-16 16:13:16 -0500

    Initial Commit

---
+++ Vibrancy/VibrancyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibrancyApp.swift
+//  Vibrancy
+//
+//  Created by Philip Davis on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct VibrancyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
