151f93984c60b37a2cacb8bc4b04b368d7d9576e
Philip Davis <philipcdavis11@gmail.com>
2021-10-13 22:45:21 -0700

    Updated for iOS15 and no dependencies

--- Vibrancy/ContentView.swift
+++ Vibrancy/ContentView.swift
@@ -5,74 +5,55 @@
 //  Created by Philip Davis on 1/14/21.
 //
 
-import SwiftUIVisualEffects
 import SwiftUI
 
 struct ContentView: View {
-    private var vibrancyLabels: [UIVibrancyEffectStyle] = [ .label, .secondaryLabel, .tertiaryLabel, .quaternaryLabel]
-    private var vibrancyLabelsNames: [String] = [ "Label", "Secondary Label", "Tertiary Label", "Quaternary Label"]
+    private var vibrancyLabelsNames: [String] = ["Primary Label", "Secondary Label", "Tertiary Label", "Quaternary Label"]
+    private var vibrancyLabelFills: [HierarchicalShapeStyle] = [.primary, .secondary, .tertiary, .quaternary]
     
-    private var vibrancyFills: [UIVibrancyEffectStyle] = [ .fill, .secondaryFill, .tertiaryFill]
-    private var vibrancyFillNames: [String] = [ "Fill", "Secondary", "Tertiary"]
     
+    private var vibrancyFills: [HierarchicalShapeStyle] = [.secondary, .tertiary, .quaternary]
+    private var vibrancyNames: [String] = ["Secondary", "Tertiary", "Quaternary"]
+    
+
     var body: some View {
         ZStack {
             Image("background")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            
-            
+                .edgesIgnoringSafeArea( .all)
             
             VStack {
-                
                 Spacer()
                 
                 ZStack {
-                    Color(.clear).blurEffect()
-                        .blurEffectStyle(.systemThinMaterial)
-                    
                     TabView {
                         HStack(spacing: 30) {
                             ForEach(0..<vibrancyFills.count) { i in
                                 VStack(spacing: 10) {
                                     Image(systemName: "swift")
                                         .font(.system(size: 60))
-                                        .vibrancyEffect()
-                                        .vibrancyEffectStyle(vibrancyFills[i])
-                                    
-                                    Text(vibrancyFillNames[i])
-                                        .fontWeight(.bold)
-                                        .vibrancyEffect()
-                                        .vibrancyEffectStyle(vibrancyFills[i])
+                                        .foregroundStyle(vibrancyFills[i])
+
+                                    Text(vibrancyNames[i])
+                                        .font(Font.headline.bold())
+                                        .foregroundStyle(vibrancyFills[i])
                                 }
                             }
                         }
                         
                         VStack(alignment: .leading) {
-                            ForEach(0..<vibrancyLabels.count) { i in
+                            ForEach(0..<vibrancyLabelFills.count) { i in
                                 Text(vibrancyLabelsNames[i])
-                                    .font(Font/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/.bold())
-                                    .vibrancyEffect()
-                                    .vibrancyEffectStyle(vibrancyLabels[i])
+                                    .font(Font.title.bold())
+                                    .foregroundStyle(vibrancyLabelFills[i])
                             }
                         }
-                        
-                        
-                        
-                        
-                    }
-                    
+                    }.background(.thinMaterial)
                     .tabViewStyle(PageTabViewStyle())
                 }.frame(height: 400)
                 
-                
-                
-                
-            }.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            
-            
-            
+            }.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/ .all/*@END_MENU_TOKEN@*/)
         }
     }
 }
@@ -80,6 +61,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            
     }
 }
